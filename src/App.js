928776388bb3618db335251de5ff90546314b5d1
minkhant <minkhant@agga.io>
2024-06-25 02:16:37 +0700

    xtwittershare

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { useEffect, useState, useRef } from "react";
 import "./App.scss";
-import { FaQuoteLeft, FaFacebookSquare} from "react-icons/fa";
+import { FaQuoteLeft, FaFacebookSquare } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 
 const App = () => {
   const [quotes, setQuotes] = useState({});
   const [color, setColor] = useState("#000000");
+  const [isLarge, setIsLarge] = useState(false); // State to control box size
   const Website_URL = "https://random-quote-machine-rouge.vercel.app/";
   const Share_Text = quotes.content;
-  const HASHTAGS_TO_INCLUDE = quotes.author;
+  const HASHTAGS_TO_INCLUDE = "#" + quotes.author;
 
   const twitterShareURL =
     "https://twitter.com/intent/tweet?url=" +
     encodeURIComponent(Website_URL) +
     "&text=" +
     encodeURIComponent(Share_Text) +
-    "&hashtags=" +
     encodeURIComponent(HASHTAGS_TO_INCLUDE);
 
   const fbShareURL =
     "https://www.facebook.com/dialog/share?" +
     "app_id=1093372268341863" +
-    "&display=popup" +
-    "&href=" + encodeURIComponent(Website_URL);
+    "&display" +
+    "&href=" +
+    encodeURIComponent(Website_URL);
 
   const buttonRef = useRef(null);
 
@@ -32,11 +33,10 @@ const App = () => {
 
   function fetchData() {
     fetch("https://api.quotable.io/random")
-      .then((res) => {
-        return res.json();
-      })
+      .then((res) => res.json())
       .then((data) => {
         setQuotes(data);
+        setIsLarge(true); // Make the box large when new quote is fetched
       });
   }
 
@@ -57,19 +57,25 @@ const App = () => {
 
   return (
     <div className="app" style={{ backgroundColor: color }}>
-      <div className="box">
+      <div
+        className={`box ${isLarge ? "large" : "small"}`}
+        data-href="https://www.facebook.com/20531316728/posts/10154009990506729/"
+        data-width="500"
+        data-show-text="true"
+      >
         <FaQuoteLeft />
         <p>{quotes.content}</p>
         <span>-{quotes.author}</span>
         <div className="group">
           <div className="icon" style={{ color: color }}>
-            <FaSquareXTwitter className="icons" href={twitterShareURL} onClick={tweet} />
-            <FaFacebookSquare className="icons" href={fbShareURL} onClick={shareOnFacebook} />
+            <FaSquareXTwitter className="icons" onClick={tweet} />
+            <FaFacebookSquare className="icons" onClick={shareOnFacebook} />
           </div>
           <button
             ref={buttonRef}
             onClick={handleClick}
-            style={{ backgroundColor: color }}>
+            style={{ backgroundColor: color }}
+          >
             Next Quote
           </button>
         </div>
